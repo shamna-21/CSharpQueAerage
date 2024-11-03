04332f8aa68d778f291aafac87a632d72e143da5
shamna <sherinshamna78@gmail.com>
2024-11-03 15:14:04 +0530

    Add project files.

---
+++ AverageInAnArray/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace AverageInAnArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter integers separated by spaces:");
+            string input = Console.ReadLine();
+            int[] arr = Array.ConvertAll(input.Split(' '), int.Parse);
+            Program prog = new Program();
+            double avg = prog.Average(arr);
+            Console.WriteLine($"average is {avg}");
+        }
+        public double Average(int[] arr)
+        {
+            int sum = 0;
+            foreach (int i in arr)
+            {
+                sum += i;
+            }
+            return (double)sum / arr.Length;
+        }
+
+    }
+}
+
